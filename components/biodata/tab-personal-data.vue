d6e36c00b33649fda4039dd32138aeed4da2f03c
Muhammad Ariyanto Wijaya <ryan.jay.7777@gmail.com>
2021-07-05 00:40:36 +0700

    [UPDATE] tab personal data using rds component

--- components/biodata/tab-personal-data.vue
+++ components/biodata/tab-personal-data.vue
@@ -3,11 +3,11 @@
     <form>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Angkatan</label>
-        <select class="form-control col-sm-8">
-          <option>
-            2020/2021
-          </option>
-        </select>
+        <rds-select class="col-sm-8" label="">
+          <rds-select-option>2020/2021</rds-select-option>
+          <rds-select-option>2021/2022</rds-select-option>
+          <rds-select-option>2022/2023</rds-select-option>
+        </rds-select>
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Photo</label>
@@ -17,122 +17,138 @@
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> NIS</label>
-        <input class="form-control col-sm-8" placeholder="NIS">
+        <rds-text-input class="col-sm-8" placeholder="NIS" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> NISN</label>
-        <input class="form-control col-sm-8" placeholder="NISN">
+        <rds-text-input class="col-sm-8" placeholder="NISN" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> NIK / No KITAS (untuk WNA)</label>
-        <input class="form-control col-sm-8" placeholder="NIK">
+        <rds-text-input class="col-sm-8" placeholder="NIK" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Nama Lengkap</label>
-        <input class="form-control col-sm-8" placeholder="Nama Lengkap">
+        <rds-text-input class="col-sm-8" placeholder="Nama Lengkap" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Nama Panggilan</label>
-        <input class="form-control col-sm-8" placeholder="Nama Panggilan">
+        <rds-text-input class="col-sm-8" placeholder="Nama Panggilan" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Tempat, Tanggal Lahir</label>
-        <input class="form-control col-sm-4" placeholder="Tempat">
-        <input class="single-daterange form-control col-sm-4" placeholder="Tanggal Lahir" type="text" value="04/12/1978">
+        <rds-text-input class="col-sm-4" placeholder="Tempat" />
+        <rds-date-picker kind="single" date-label="" class="customDatePicker col-sm-4" :value="new Date()" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Jenis Kelamin</label>
         <div class="col-sm-8">
-          <div class="form-check">
-            <label class="form-check-label"><input checked="" class="form-check-input" name="rdGender" type="radio" value="male">Laki-laki</label>
-          </div>
-          <div class="form-check">
-            <label class="form-check-label"><input class="form-check-input" name="rdGender" type="radio" value="female">Perempuan</label>
-          </div>
+          <rds-radio-group>
+            <rds-radio-button name="rdGender" label="Laki-laki" value="male" :label-left="false" />
+            <rds-radio-button name="rdGender" label="Perempuan" value="female" :label-left="false" />
+          </rds-radio-group>
         </div>
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Agama</label>
-        <select class="form-control col-sm-8">
-          <option>Islam</option>
-          <option>Katolik</option>
-          <option>Kristen</option>
-          <option>Hindu</option>
-          <option>Budha</option>
-          <option>Other</option>
-        </select>
+        <rds-select class="col-sm-8" label="">
+          <rds-select-option value="islam">Islam</rds-select-option>
+          <rds-select-option value="catholic">Katolik</rds-select-option>
+          <rds-select-option value="christian">Kristen</rds-select-option>
+          <rds-select-option value="hinduism">Hindu</rds-select-option>
+          <rds-select-option value="buddhism">Buddha</rds-select-option>
+          <rds-select-option value="other">Other</rds-select-option>
+        </rds-select>
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Handphone</label>
-        <input class="form-control col-sm-8" placeholder="Handphone">
+        <rds-text-input class="col-sm-8" placeholder="Handphone" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Website / Blog</label>
-        <input class="form-control col-sm-8" placeholder="Website / Blog">
+        <rds-text-input class="col-sm-8" placeholder="Website / Blog" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Kewarganegaraan</label>
         <div class="col-sm-8">
-          <div class="form-check">
-            <label class="form-check-label"><input checked="" class="form-check-input" name="rdNationality" type="radio" value="wni">WNI</label>
-          </div>
-          <div class="form-check">
-            <label class="form-check-label"><input class="form-check-input" name="rdNationality" type="radio" value="wna">WNA</label>
-          </div>
+          <rds-radio-group>
+            <rds-radio-button name="rdNationality" label="WNI" value="ID" :label-left="false" />
+            <rds-radio-button name="rdNationality" label="WNA" value="WNA" :label-left="false" />
+          </rds-radio-group>
         </div>
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Anak Ke</label>
-        <input class="form-control col-sm-8" placeholder="Anak Ke" type="number">
+        <rds-text-input class="col-sm-8" placeholder="Anak Ke" type="number" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Jumlah Saudara Kandung</label>
-        <input class="form-control col-sm-8" placeholder="Jumlah Saudara Kandung" type="number">
+        <rds-text-input class="col-sm-8" placeholder="Jumlah Saudara Kandung" type="number" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Jumlah Saudara Tiru</label>
-        <input class="form-control col-sm-8" placeholder="Jumlah Saudara Tiri" type="number">
+        <rds-text-input class="col-sm-8" placeholder="Jumlah Saudara Tiri" type="number" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Jumlah Saudara Angkat</label>
-        <input class="form-control col-sm-8" placeholder="Jumlah Saudara Angkat" type="number">
+        <rds-text-input class="col-sm-8" placeholder="Jumlah Saudara Angkat" type="number" />
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Bahasa Sehari-hari</label>
-        <select class="form-control col-sm-8">
-          <option value="Bahasa Indonesia">Bahasa Indonesia</option>
-          <option value="Bahasa Inggris">Bahasa Inggris</option>
-          <option value="Bahasa Jawa">Bahasa Jawa</option>
-          <option value="Bahasa Arab">Bahasa Arab</option>
-          <option value="Bahasa Sunda">Bahasa Sunda</option>
-          <option value="Bahasa Madura">Bahasa Madura</option>
-          <option value="Bahasa Melayu">Bahasa Melayu</option>
-        </select>
+        <rds-select class="col-sm-8" label="">
+          <rds-select-option value="Bahasa Indonesia">Bahasa Indonesia</rds-select-option>
+          <rds-select-option value="Bahasa Inggris">Bahasa Inggris</rds-select-option>
+          <rds-select-option value="Bahasa Jawa">Bahasa Jawa</rds-select-option>
+          <rds-select-option value="Bahasa Arab">Bahasa Arab</rds-select-option>
+          <rds-select-option value="Bahasa Sunda">Bahasa Sunda</rds-select-option>
+          <rds-select-option value="Bahasa Madura">Bahasa Madura</rds-select-option>
+          <rds-select-option value="Bahasa Melayu">Bahasa Melayu</rds-select-option>
+        </rds-select>
       </div>
       <div class="form-group row">
         <label class="col-sm-4" for=""> Status Keluarga</label>
-        <select class="form-control col-sm-8">
-          <option value="intact">Lengkap</option>
-          <option value="orphaned">Yatim Piatu</option>
-          <option value="orphaned-without-father">Yatim</option>
-          <option value="orphaned-without-mother">Piatu</option>
-        </select>
+        <rds-select class="col-sm-8" label="">
+          <rds-select-option value="intact">Lengkap</rds-select-option>
+          <rds-select-option value="orphaned">Yatim Piatu</rds-select-option>
+          <rds-select-option value="orphaned-without-father">Yatim</rds-select-option>
+          <rds-select-option value="orphaned-without-mother">Piatu</rds-select-option>
+        </rds-select>
       </div>
       <div class="form-buttons-w">
-        <button class="btn btn-primary" type="button"> Update</button>
+        <rds-button kind="primary" size="small"> Update</rds-button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import { RdsTextInput } from '@/.design-system/core/components/rds-text-input/';
+import { RdsSelect, RdsSelectOption } from '@/.design-system/core/components/rds-select/';
+import { RdsRadioGroup, RdsRadioButton } from '@/.design-system/core/components/rds-radio-button/';
+import { RdsDatePicker } from '@/.design-system/core/components/rds-date-picker/';
+import { RdsButton } from '@/.design-system/core/components/rds-button/';
+
 export default {
-  name: 'TabPersonalData'
+  name: 'TabPersonalData',
+  components: {
+    RdsTextInput,
+    RdsSelect,
+    RdsSelectOption,
+    RdsRadioGroup,
+    RdsRadioButton,
+    RdsDatePicker,
+    RdsButton
+  }
 }
 </script>
 
 <style scoped>
 .tab-section {
   padding: 20px;
+}
+.customDatePicker >>> .bx--date-picker,
+.customDatePicker >>> .bx--date-picker-container,
+.customDatePicker >>> .bx--date-picker__input {
+  width: 100% !important;
 }
 </style>

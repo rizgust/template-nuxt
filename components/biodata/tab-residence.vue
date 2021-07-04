2d55527cd7c9bf53197d79ab1719564e3d176ffe
Muhammad Ariyanto Wijaya <ryan.jay.7777@gmail.com>
2021-07-05 00:55:30 +0700

    [UPDATE] tab residence using rds component

--- components/biodata/tab-residence.vue
+++ components/biodata/tab-residence.vue
@@ -5,66 +5,73 @@
         <div class="col-md-6">
           <div class="form-group row">
             <label class="col-sm-4" for=""> Alamat Rumah</label>
-            <input class="form-control col-sm-8" placeholder="Alamat Rumah">
+            <rds-text-input class="col-sm-8" placeholder="Alamat Rumah" />
           </div>
           <div class="form-group row">
             <label class="col-sm-4" for=""> RT / RW</label>
-            <input class="form-control col-sm-8" placeholder="RT / RW">
+            <rds-text-input class="col-sm-8" placeholder="RT / RW" />
           </div>
           <div class="form-group row">
             <label class="col-sm-4" for=""> Kode Pos</label>
-            <input class="form-control col-sm-8" placeholder="Kode Pos">
+            <rds-text-input class="col-sm-8" placeholder="Kode Pos" />
           </div>
           <div class="form-group row">
             <label class="col-sm-4" for=""> Provinsi</label>
-            <input class="form-control col-sm-8" placeholder="Provinsi">
+            <rds-text-input class="col-sm-8" placeholder="Provinsi" />
           </div>
           <div class="form-group row">
             <label class="col-sm-4" for=""> Kota / Kabupaten</label>
-            <input class="form-control col-sm-8" placeholder="Kota / Kabupaten">
+            <rds-text-input class="col-sm-8" placeholder="Kota / Kabupaten" />
           </div>
           <div class="form-group row">
             <label class="col-sm-4" for=""> Kecamatan</label>
-            <input class="form-control col-sm-8" placeholder="Kecamatan">
+            <rds-text-input class="col-sm-8" placeholder="Kecamatan" />
           </div>
           <div class="form-group row">
             <label class="col-sm-4" for=""> Kelurahan</label>
-            <input class="form-control col-sm-8" placeholder="Kelurahan">
+            <rds-text-input class="col-sm-8" placeholder="Kelurahan" />
           </div>
         </div>
         <div class="col-md-6">
           <div class="form-group row">
             <label class="col-sm-4" for=""> Telepon Rumah</label>
-            <input class="form-control col-sm-8" placeholder="Telepon Rumah">
+            <rds-text-input class="col-sm-8" placeholder="Telepon Rumah" />
           </div>
           <div class="form-group row">
             <label class="col-sm-4" for=""> Tinggal dengan</label>
-            <input class="form-control col-sm-8" placeholder="Tinggal dengan">
+            <rds-text-input class="col-sm-8" placeholder="Tinggal dengan" />
           </div>
           <div class="form-group row">
             <label class="col-sm-4" for=""> Jarak dari Rumah ke Sekolah</label>
-            <input class="form-control col-sm-8" placeholder="Jarak dari Rumah ke Sekolah">
+            <rds-text-input class="col-sm-8" placeholder="Jarak dari Rumah ke Sekolah" />
           </div>
           <div class="form-group row">
             <label class="col-sm-4" for=""> Berangkat ke Sekolah</label>
-            <input class="form-control col-sm-8" placeholder="Berangkat ke Sekolah">
+            <rds-text-input class="col-sm-8" placeholder="Berangkat ke Sekolah" />
           </div>
           <div class="form-group row">
             <label class="col-sm-4" for=""> Ruang Tidur</label>
-            <input class="form-control col-sm-8" placeholder="Ruang Tidur">
+            <rds-text-input class="col-sm-8" placeholder="Ruang Tidur" />
           </div>
         </div>
       </div>
       <div class="form-buttons-w">
-        <button class="btn btn-primary" type="button"> Update</button>
+        <rds-button kind="primary" size="small"> Update</rds-button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import { RdsTextInput } from '@/.design-system/core/components/rds-text-input/';
+import { RdsButton } from '@/.design-system/core/components/rds-button/';
+
 export default {
-  name: 'TabResidence'
+  name: 'TabResidence',
+  components: {
+    RdsTextInput,
+    RdsButton
+  }
 }
 </script>
 
